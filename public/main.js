26e2ed6a2403e43942019917f62b2cfb7624beb2
suecollarte <suecollarte@gmail.com>
2022-09-08 12:56:49 -0300

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,14 +1,16 @@
 const socket = io();
 //PARTE CLIENTE
-socket.on('from-server-producto', datos => {
-    //console.log('productos:', producto.PRODUCTO_DB);
-    if (!datos.PRODUCTO_DB.lenght){
-        renderProdVacio();
-        return;
-    }
-    renderProd(producto.PRODUCTO_DB); 
+socket.on('from-server-producto', (datos) => {
+    
+    if (datos.PRODUCTO_DB.lenght ==0){
+       renderProdVacio();
+       return;
+    } 
+  
+    renderProd(datos.PRODUCTO_DB); 
     
 });
+
 
 function renderProdVacio() {
     const htmlCuerpo = '<h3 class="alert alert-warning">No hay productos</h3>';
@@ -20,11 +22,15 @@ function renderProd(productos) {
      return (`<tr>
      <td>${prod.nombre}</td>
      <td>${prod.precio}</td>
-     <td><img src="${prod.foto}" width="50px"></td><tr>`)
+     <td><img src="${prod.imagenPro}" width="50px"></td><tr>`)
     }).join(''); 
     
   const table =
-  '<div class="table-responsive"><table class="table table-dark"><tr style="color: yellow;"> <th>Nombre</th> <th>Precio</th> <th>Imagen</th> </tr>' +
+  '<div class="table-responsive">'+
+  '<table class="table table-dark">'+
+  '<tr style="color: yellow;"> '+
+  ' <th>Nombre</th> <th>Precio</th>'+   
+  '<th>Imagen</th> </tr>' +
   HistoriaProd +
   '</table></div>';
     document.querySelector('#historial').innerHTML = table;
@@ -34,46 +40,73 @@ function enviarProducto() {
    
     const inputProducto = document.querySelector('#nombre');
     const inputPrecio = document.querySelector('#precio');
-    const inputThumbail = document.querySelector('#foto');
+    const inputimagenPro = document.querySelector('#imagenProd');
+    const inputDescrProducto = document.querySelector('#descrProducto');
+    const inputStock = document.querySelector('#stockProd');
+    const inputid = document.querySelector('#idProducto');
 
  
     const Fproducto = {
         nombre: inputProducto.value,
         precio: inputPrecio.value,
-        foto : inputThumbail.value
+        descrpro: inputDescrProducto.value,
+        stock: inputStock.value,
+        id: inputid.value,
+        imagenPro : inputimagenPro.value
     }
 
    socket.emit('from-client-producto', Fproducto);
 }
 
 
-
-socket.on('from-server-carrito', carrito => {
-    console.log('Usuarios:', carrito.CARRITO_DB);
-    renderCarrito(carrito.CARRITO_DB); 
+socket.on('from-server-carrito', (datos) => {
+    
+    if (datos.CARRITO_DB.lenght ==0){
+        renderCarrVacio();
+        return;
+     } 
+    renderCarrito(datos.CARRITO_DB); 
     
 });
+
+function renderCarrVacio() {
+    const htmlCuerpo = '<h3 class="alert alert-warning">No hay </h3>';
+    document.querySelector('#carritos').innerHTML = htmlCuerpo;
+  }
+
 function renderCarrito(carrito) {
-    const HistoriaMsj= carrito.map((msj)=>{
-        return (`<span style='color:blue;'>
-        <b>${msj.nombre}: </b>
-        <span style='color:#804000;'>${msj.descripcion}</span>
-        <span style='color:green;font-style: italic;'>${msj.stock} </span>`)
-    }).join('<br>'); 
-    document.querySelector('#carritos').innerHTML = HistoriaMsj;
-}
-function enviarCarrito(e) {
     
-    const inputid = document.querySelector('#id');
-    const inputnombre = document.querySelector('#nombre');
-    const inputdescripcion = document.querySelector('#descripcion');
-    const inputcodigo = document.querySelector('#codigo');
-    const inputstock = document.querySelector('#stock');
-    const inputprecio = document.querySelector('#precio');
-    const inputfoto = document.querySelector('#foto');
+    const HtmlCarrito= carrito.map((carro)=>{
+        
+        return (`<tr><td style='color:blue;'>
+        <b>${carro.nombreC}: </b></td>
+        <td style='color:#804000;'>${carro.descripcionC}</td>
+        <td style='color:green;font-style: italic;'>${carro.stockC} </td></tr>`)
+    }).join(''); 
+    const tabla =
+    '<div class="table-responsive">'+
+    '<table class="table table-dark">'+
+    '<tr style="color: yellow;"> '+
+    ' <th>Nombre</th> <th>Descripcion</th>'+   
+    '<th>Stock</th> </tr>' +
+    HtmlCarrito +
+    '</table></div>';
+    document.querySelector('#carritos').innerHTML = tabla;
+}
+
+
+function enviarCarrito() {
+    
+   
+    const inputnombre = document.querySelector('#nombreC');
+    const inputdescripcion = document.querySelector('#descripcionC');
+    const inputcodigo = document.querySelector('#codigoC');
+    const inputstock = document.querySelector('#stockC');
+    const inputprecio = document.querySelector('#precioC');
+    const inputfoto = document.querySelector('#fotoC');
     const date= new Date();
     const usuarios = {
-        id: inputid.value,
+        id: 1,
         timestamp: date.toLocaleString(),
         nombre: inputnombre.value,
         descripcion: inputdescripcion.value, 
@@ -82,7 +115,5 @@ function enviarCarrito(e) {
         precio: inputprecio.value, 
         foto: inputfoto.value
     }
-    
-    socket.emit('from-client-carrito', usuarios);
-    return false;
+   socket.emit('from-client-carrito', usuarios);
 }
